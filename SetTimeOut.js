bee983f1d89f20c4bf1a22aeff19faa4340073e1
rajmal varma <rajmalvarma9@gmail.com>
2023-07-14 17:36:06 +0530

    set time out method

---
+++ SetTimeOut.js
@@ -0,0 +1,41 @@
+//window object - it represents an open window in browser 
+//window object is the root of the hierarchy of objects
+//window object is the global object in client side javascript
+
+// window object name - in browser it is window
+// in nodejs it is global
+
+//Set time out function- it is used to execute a function after a specified time interval  in milliseconds  - 1000 milliseconds = 1 second  
+//set time out function is a method of window object.
+
+//set time out function takes two parameters - 1. function to be executed 2. time interval in milliseconds
+
+//set time out function returns a number which is the id of the timer
+
+
+//Example 1
+// function sum(a,b) {
+//     console.log(a+b);
+// }
+// setTimeout(() => {
+//     console.log("Hello World");  // this function will be executed after 4 seconds
+// }, 4000);
+
+// sum(10,20);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
