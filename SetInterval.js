44721be27e0be7384abcd7b5c8b3fc1fb7e6c6f4
rajmal varma <rajmalvarma9@gmail.com>
2023-07-14 17:37:03 +0530

    set interval window object meyhod

---
+++ SetInterval.js
@@ -0,0 +1,19 @@
+// set interval function - it is used to execute a function repeatedly after a specified time interval  in milliseconds  - 1000 milliseconds = 1 second
+// set interval function is a method of window object.
+
+// set interval function takes two parameters - 1. function to be executed 2. time interval in milliseconds
+
+// set interval function returns a number which is the id of the timer
+
+// Example 1
+
+
+
+// setInterval(() =>{
+//     console.log(sum(10,20));
+//     console.log("Hello World");  // this function will be executed after 4 seconds repeatedly
+//     function sum (a,b){
+//         return a+b;
+//     }
+   
+// },2000);
